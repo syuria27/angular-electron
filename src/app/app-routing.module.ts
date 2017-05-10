99c2d8de40981e8239b699f689e97453026088cd
Syukron Rizqi Ardhi <syukron.tkj2@gmail.com>
2017-05-10 14:16:38 +0700

    add routing siswa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './components/login/login.component';
-
+import { ListSiswaComponent } from './components/list-siswa/list-siswa.component';
+import { CreateSiswaComponent } from './components/create-siswa/create-siswa.component';
+import { EditSiswaComponent } from './components/edit-siswa/edit-siswa.component';
 
 const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'login' },
     { path: 'login', component: LoginComponent },
     { path: 'home', component: HomeComponent },
+    { path: 'siswa/list', component: ListSiswaComponent },
+    { path: 'siswa/create', component: CreateSiswaComponent },
+    { path: 'siswa/edit/:nis', component: EditSiswaComponent },
 ];
 
 @NgModule({
